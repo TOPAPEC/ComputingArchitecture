2f7c62a0d9d05863123fc8b149b3cd9b010d24fa
Topapec <danbugrienko@gmail.com>
2021-12-12 23:43:53 +0300

    Debugging

--- hw5/main.cpp
+++ hw5/main.cpp
@@ -25,16 +25,16 @@ void *eat_think_repeat(void *arg) {
             }
             int thinking_duration = rand() % 100 + 1;
             pthread_mutex_lock(&output_mutex);
-            cout << phil_id << " thinks " << thinking_duration << "ms\n";
-            cout << phil_id << " waiting for " << phil_id << " fork.\n";
+//            cout << phil_id << " thinks " << thinking_duration << "ms\n";
+//            cout << phil_id << " waiting for " << phil_id << " fork.\n";
             pthread_mutex_unlock(&output_mutex);
             sem_wait(&sems[phil_id]);
             pthread_mutex_lock(&output_mutex);
-            cout << phil_id << " grabbed " << phil_id << " fork. Waiting for " << phil_id + 1 << " fork.\n";
+//            cout << phil_id << " grabbed " << phil_id << " fork. Waiting for " << phil_id + 1 << " fork.\n";
             pthread_mutex_unlock(&output_mutex);
             sem_wait(&sems[phil_id + 1]);
             pthread_mutex_lock(&output_mutex);
-            cout << phil_id << " got both forks! Beginning eating immediately!\n";
+//            cout << phil_id << " got both forks! Beginning eating immediately!\n";
             pthread_mutex_unlock(&output_mutex);
             sem_post(&sems[phil_id]);
             sem_post(&sems[phil_id + 1]);
