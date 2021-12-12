c4e07c61677009087ad89ff20a3902b92ce6c252
Topapec <danbugrienko@gmail.com>
2021-12-12 23:01:49 +0300

    Debugging

--- hw5/main.cpp
+++ hw5/main.cpp
@@ -12,6 +12,7 @@ using namespace std;
 sem_t *sems;
 bool terminate_checkpoint;
 pthread_mutex_t output_mutex;
+pthread_mutex_t terminate_mutex;
 void run(int phil_num, int dinner_duration);
 
 // Arg - philosopher id.
@@ -48,6 +49,7 @@ void *eat_think_repeat(void *arg) {
         cout << "Now duration of dinner in seconds:";
         cin >> dinner_duration;
         pthread_mutex_init(&output_mutex, NULL);
+        pthread_mutex_init(&terminate_mutex, NULL);
         sems = new sem_t[phil_num];
         run(phil_num, dinner_duration);
         cout << "Dinner is over!\n";
@@ -62,7 +64,9 @@ void *eat_think_repeat(void *arg) {
             pthread_create(&phil_array[i], NULL, &eat_think_repeat, &i);
         }
         this_thread::sleep_for(chrono::milliseconds(dinner_duration * 1000));
+        pthread_mutex_lock(&terminate_mutex);
         terminate_checkpoint = true;
+        pthread_mutex_unlock(&terminate_mutex);
         for (int i = 0; i < phil_num; ++i) {
             pthread_join(phil_array[i], NULL);
         }
